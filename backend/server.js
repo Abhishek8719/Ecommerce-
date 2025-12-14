75353f55e0163eba6b40488cef395982186074fd
Abhishek <abhisekchoudhary31@gmail.com>
2025-12-14 17:11:26 +0530

    integrated with Mongodb and Product model with product api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const cors = require("cors")
-
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Product from "./models/product.js";
 const app = express();
+
 app.use(
     cors({
         origin :"http://localhost:5173"
@@ -9,21 +11,45 @@ app.use(
 )
 app.use(express.json());
 
-const products = [
-    {id : 1,name:"Laptop",price :55000},
-    {id:2,name :"Phone",price :25000},
-    {id:3,name:"Headphones",price :30000},
-    {id :4,name :"keyboard",price: 1500},
-    {id :5,name :"Aamla juice",price:400}
+mongoose.connect("mongodb://127.0.0.1:27017/amazonClone")
+.then(()=> console.log("MongoDb connected"))
+.catch((err)=> console.error(err));
 
-]
+
 app.get("/",(req,res)=>{
     res.send("Amazon clone backend running...")
 })
 
-app.get("/api/products",(req,res)=>{
-    res.json(products);
+
+app.post("/api/products/seed",async (req,res)=>{
+    try{
+
+
+        const productData = [
+            {"name":"Laptop","price":23000},
+            {"name":"Phone","price":34000},
+            {"name":"iPhone","price":110000},
+            {"name":"LCD","price":80000}
+         ]        
+
+         await Product.insertMany(productData);
+         res.json({message:"Products seeded successfully"})
+    } catch(err){
+       res.status(500).json({message:"Error seeding data", error: err.message});
+    }
+
 })
+
+app.get("/api/products",async (req,res)=>{
+    try{
+        const products = await Product.find();
+        res.json(products);
+    }catch(err){
+        res.status(500).json({message:"server error"});
+    }
+    
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Backend server running on the http://localhost:${PORT}`);
